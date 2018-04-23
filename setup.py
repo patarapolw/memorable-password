55438e767072436c3067943c6bd29e96e6cf1ea1
patarapolw <patarapolw@gmail.com>
2018-04-23 10:18:42 +0700

    make it better

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-install_requires = ['nltk', 'PyYAML', 'markovify', 'flask', 'kivy']
+install_requires = ['nltk', 'PyYAML', 'markovify', 'flask']
+android_requires = ['kivy']
 pytest_requires = ['xdist', 'repeat', 'timeout', 'doctestplus']
 tests_require = ['pytest'] + ['pytest-{}'.format(req) for req in pytest_requires]
 
@@ -51,6 +52,7 @@ setup(
     tests_require=pytest_requires,
     extras_require={  # Optional
         'test': ['tox'] + pytest_requires,
+        'android'
         'with-language-check': ['language-check']
     },
     package_data={  # Optional
