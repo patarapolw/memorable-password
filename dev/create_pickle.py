43658eca8c861cbe7990769e00b37e37766559b7
patarapolw <patarapolw@gmail.com>
2018-04-23 12:23:31 +0700

    Pickle GeneratePassword, for the web
    Former-commit-id: 7b894f9784079e50defc0b26bf21fbb96386e767

--- dev/create_pickle.py
+++ dev/create_pickle.py
@@ -5,4 +5,4 @@ from memorable_password import GeneratePassword
 
 if __name__ == '__main__':
     with open('../generate_password.pkl', 'wb') as f:
-        pickle.dump(GeneratePassword(do_markovify=False), f)
+        pickle.dump(GeneratePassword(do_markovify=True), f)
