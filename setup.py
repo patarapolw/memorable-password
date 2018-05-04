6a42503769181777a67af2d8fcdc423fecb767e5
patarapolw <patarapolw@gmail.com>
2018-05-04 14:04:53 +0700

    This repo is for PyPI, separate the password manager component

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-install_requires = ['nltk', 'PyYAML', 'markovify', 'randomsentence', 'diceware_utils', 'password_manager']
+install_requires = ['nltk', 'PyYAML', 'markovify', 'randomsentence', 'diceware_utils']
 web_requires = ['flask']
 pytest_requires = ['xdist', 'repeat', 'timeout', 'doctestplus']
 tests_require = ['pytest', 'passwordstrength', 'pronounceable'] \
